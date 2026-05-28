2688f5f3a7a9d879ff4ff991df4e703b7ed992f2
sepehrsylvanus <mr.forte.world@gmail.com>
2026-05-28 18:23:35 +0330

    Create a theme toggler

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import Theme from "./Theme";
 
 const Navbar = () => {
   return (
@@ -18,7 +19,9 @@ const Navbar = () => {
 
       <p>Global Search</p>
 
-      <div className="flex-between gap-5">Theme</div>
+      <div className="flex-between gap-5">
+        <Theme />
+      </div>
     </nav>
   );
 };
